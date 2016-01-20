207731036c2e878e99cc68bfd1d9c7ec559f42e8
Danil <busymangorbenko@gmail.com>
2016-01-20 13:05:58 +0200

    added AuthenticationService to authentication module dependencies

--- app/modules/authentication/index.js
+++ app/modules/authentication/index.js
@@ -1,13 +1,31 @@
 import angular from 'angular';
 
+/**
+ * Controllers
+ */
+
 import SignInController from './controllers/sign_in_controller';
-//import SignUpController from './controllers/sign_up_controller';
+
+/**
+ * Interceptors
+ */
 
 import NotAuthorizedInterceptor from './services/not_authorized_interceptor';
 
+/**
+ * Services
+ */
+
+import AuthenticationService from './services/authentication_service';
+
+/**
+ * Definition
+ */
+
 export default angular.module('app.authentication', [])
   .controller('SignInController', SignInController)
-  //.controller('SignUpController', SignUpController)
 
   .factory('NotAuthorizedInterceptor', NotAuthorizedInterceptor)
+
+  .service('AuthenticationService', AuthenticationService)
   .name;
