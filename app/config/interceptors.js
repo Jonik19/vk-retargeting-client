00ef1064919974912a0eca0a48daba4aa42f74ca
Danil <busymangorbenko@gmail.com>
2016-01-20 18:20:43 +0200

    using TokenInterceptor

--- app/config/interceptors.js
+++ app/config/interceptors.js
@@ -2,16 +2,9 @@
  * List of http interceptors
  */
 
-/**
- * !!!!!!!!!!!!!!!!!
- * Need to decide, require interceptor or pass name of factory.
- * !!!!!!!!!!!!!!!!!
- */
-
-import NotAuthorizedInterceptor from '../modules/authentication/services/not_authorized_interceptor';
-
 export default function interceptors($httpProvider) {
   $httpProvider.interceptors.push('NotAuthorizedInterceptor');
+  $httpProvider.interceptors.push('TokenInterceptor');
 };
 
 interceptors.$inject = ['$httpProvider'];
