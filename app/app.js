15c1e45de2457fa23e846b124da14690ddd2b3ec
Danil <busymangorbenko@gmail.com>
2016-01-20 18:20:04 +0200

    added script on application run

--- app/app.js
+++ app/app.js
@@ -6,11 +6,14 @@ import uiRouter from 'angular-ui-router';
 import routes from './config/routes';
 import interceptors from './config/interceptors';
 import http from './config/http';
+import run from './config/run';
 
 import home from './modules/home';
 import authentication from './modules/authentication';
+import common from './modules/common';
 
-angular.module('app', [uiRouter, home, authentication])
+angular.module('app', [uiRouter, home, authentication, common])
   .config(interceptors)
   .config(http)
-  .config(routes);
+  .config(routes)
+  .run(run);
