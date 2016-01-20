bf4f63196c390d1df6aa3f0354cbe2cf081f03fe
Danil <busymangorbenko@gmail.com>
2016-01-20 18:19:23 +0200

    added docs to sign_in_controller

--- app/modules/authentication/controllers/sign_in_controller.js
+++ app/modules/authentication/controllers/sign_in_controller.js
@@ -1,5 +1,9 @@
 import Controller from '../../common/controllers/controller';
 
+/**
+ * SignInController controller.
+ */
+
 export default class SignInController extends Controller {
   constructor() {
     super(arguments);
@@ -7,15 +11,34 @@ export default class SignInController extends Controller {
     this.user = {};
   }
 
+  /**
+   * Authenticates user if successful redirects to admin page
+   * otherwise show errors.
+   *
+   * @param {Object} user Object with fields username and password
+   */
+
   signIn(user) {
     this.injections.AuthenticationService.authenticate(user)
       .then(this.redirectOnSuccess.bind(this))
       .catch(this.showErrors.bind(this));
   }
 
+  /**
+   * Method which is called on successful authentication
+   *
+   * @param user
+   */
+
   redirectOnSuccess(user) {
     this.injections.$state.go('home');
   }
+
+  /**
+   * Method which is called on unsuccessful authentication
+   *
+   * @param error
+   */
 
   showErrors(error) {
     alert(JSON.stringify(error));
