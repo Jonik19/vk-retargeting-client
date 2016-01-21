ff1d958c684c6b94a3fdbc6db7246b283604a84c
Danil <busymangorbenko@gmail.com>
2016-01-21 13:52:14 +0200

    added RoomsIndexController

---
+++ app/modules/admin/modules/rooms/controllers/rooms_index_controller.js
@@ -0,0 +1,15 @@
+import Controller from '../../../../common/controllers/controller';
+
+/**
+ * SignInController controller.
+ */
+
+export default class IndexController extends Controller {
+  constructor() {
+    super(arguments);
+  }
+
+
+}
+
+IndexController.$inject = [];
