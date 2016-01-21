3442b321b2a2caa6a30ba3063c2fc423a8fb4df2
Danil <busymangorbenko@gmail.com>
2016-01-21 13:52:36 +0200

    added RoomsCreateController

---
+++ app/modules/admin/modules/rooms/controllers/rooms_create_controller.js
@@ -0,0 +1,17 @@
+import Controller from '../../../../common/controllers/controller';
+
+/**
+ * SignInController controller.
+ */
+
+export default class RoomsIndexController extends Controller {
+  constructor() {
+    super(arguments);
+  }
+
+  create(room) {
+    alert('Hello ');
+  }
+}
+
+RoomsIndexController.$inject = [];
