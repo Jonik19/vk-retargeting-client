8e78f70032371c8f25e048245df5770e82e4b0e8
Danil <busymangorbenko@gmail.com>
2016-01-22 17:08:25 +0200

    added RoomResource#enter action

--- app/modules/admin/modules/rooms/resources/rooms_room_resource.js
+++ app/modules/admin/modules/rooms/resources/rooms_room_resource.js
@@ -3,7 +3,9 @@
  */
 
 export default function RoomsRoomResource($resource, config) {
-  return $resource(config.api.baseUrl + '/rooms/:id', { id: '@id' });
+  return $resource(config.api.baseUrl + '/rooms/:id', { id: '@id' }, {
+    enter: {method: 'post', isArray: false, url: config.api.baseUrl + '/rooms/enter'}
+  });
 };
 
 RoomsRoomResource.$inject = ['$resource', 'config'];
