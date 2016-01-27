c4a29bb35ca662b9bb6b37a7c12dde192203abb4
Danil <busymangorbenko@gmail.com>
2016-01-27 18:59:20 +0200

    edited views in rooms

--- app/modules/admin/modules/rooms/index.js
+++ app/modules/admin/modules/rooms/index.js
@@ -7,21 +7,16 @@ import angular from 'angular';
 import RoomsListController from './controllers/rooms_list_controller';
 import RoomsCreateController from './controllers/rooms_create_controller';
 import RoomsEnterController from './controllers/rooms_enter_controller';
-
-/**
- * Services
- */
-
-import RoomsRoomResource from './resources/rooms_room_resource';
+import RoomsShowController from './controllers/rooms_show_controller';
 
 /**
  * Definition
  */
 
 export default angular.module('app.admin.rooms', [])
-  .factory('RoomsRoomResource', RoomsRoomResource)
 
   .controller('RoomsListController', RoomsListController)
   .controller('RoomsCreateController', RoomsCreateController)
   .controller('RoomsEnterController', RoomsEnterController)
+  .controller('RoomsShowController', RoomsShowController)
   .name;
