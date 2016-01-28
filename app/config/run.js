e92bcac8ae0098c41c4e5b75754aebdfb326c637
Danil <busymangorbenko@gmail.com>
2016-01-28 13:09:59 +0200

    using state_change_service in application run

--- app/config/run.js
+++ app/config/run.js
@@ -2,15 +2,23 @@
  * Run
  */
 
-export default function run(AuthenticationService, $state, $rootScope, config) {
+export default function run(AuthenticationService, $state, $rootScope, config, StateChangeService) {
   $rootScope.config = config;
   $rootScope.$state = $state;
 
-  AuthenticationService.check()
-    .then(function (user) {
-      // TODO: redirect to admin page
-      $state.go('admin.rooms.list');
-    });
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+
+    // Manage access to protected and only-unprotected pages.
+
+    StateChangeService.check()
+      .finally(function () {
+        // manage access when user authentication state is resolved from api.
+
+        // it can be invoked by check method, but I think this approach is more visual
+        StateChangeService.onProtected(event, toState, toParams, fromState, fromParams);
+        StateChangeService.onUnprotectedOnly(event, toState, toParams, fromState, fromParams);
+      });
+  });
 };
 
-run.$inject = ['AuthenticationService', '$state', '$rootScope', 'config'];
+run.$inject = ['AuthenticationService', '$state', '$rootScope', 'config', 'StateChangeService'];
