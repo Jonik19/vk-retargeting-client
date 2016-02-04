5573ee59803aa7646b38f229eb80e790f75c4d09
Danil <busymangorbenko@gmail.com>
2016-02-04 13:31:03 +0200

    using html5mode

--- app/config/html5mode.js
+++ app/config/html5mode.js
@@ -1,5 +1,5 @@
 export default function html5mode($locationProvider) {
-  //$locationProvider.html5Mode(true).hashPrefix('!');
+  $locationProvider.html5Mode(true);
 }
 
 html5mode.$inject = ['$locationProvider'];
